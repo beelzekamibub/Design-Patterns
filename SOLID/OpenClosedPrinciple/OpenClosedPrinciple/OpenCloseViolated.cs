1535b855276e352889ba298e71f75a6050479cdd
beelzekamibub <chait8126@gmail.com>
2023-08-19 22:35:35 +0530

    open closed principle

---
+++ SOLID/OpenClosedPrinciple/OpenClosedPrinciple/OpenCloseViolated.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenClosedPrinciple
+{
+    public enum Colorv
+    {
+        Red,Green,Blue
+    }
+    public enum Sizev
+    {
+        Small,Medium,Large,Yuge
+    }
+    public class Productv
+    {
+        public string Name;
+        public Color Color;
+        public Size Size;
+        public Productv(string name,Color color, Size size)
+        {
+            if(name==null)
+                throw new ArgumentNullException(paramName:nameof(name));
+            Name = name;
+            Color = color;
+            Size = size;
+        }
+    }
+    public class ProductFilterv
+    {
+        public  IEnumerable<Product> FilterBySizev(IEnumerable<Product> products,Size size) 
+        {
+            foreach(Product product in products)
+            {
+                if(product.Size == size)
+                    yield return product;
+            }
+        }//to filter by color we have to come back to this class and modify this class, therefor it was not closed for editing 
+        public  IEnumerable<Product> FilterByColor(IEnumerable<Product> products, Color color)
+        {
+            foreach (Product product in products)
+            {
+                if (product.Color == color)
+                    yield return product;
+            }
+        }
+        //now if we have to filter by size and color we have to create a new function, as we dont have a generalized
+        public IEnumerable<Product> FilterByColorandSize(IEnumerable<Product> products, Color color,Size size)
+        {
+            foreach (Product product in products)
+            {
+                if (product.Color == color && product.Size==size)
+                    yield return product;
+            }
+        }
+    }
+    public class OpenCloseViolated
+    {
+        public void UseOpenClosedViolated() 
+        {
+            var apple = new Product("Apple", Color.Green, Size.Small);
+            var Tree = new Product("Tree", Color.Green, Size.Yuge);
+            var Guitar = new Product("Guitar", Color.Red, Size.Large);
+            Product[] products= new Product[]{apple, Tree, Guitar};
+            var pf = new ProductFilterv();
+            Console.WriteLine("Green products (old):");
+            foreach(var p in pf.FilterByColor(products, Color.Green))
+            {
+                Console.WriteLine(p.Name);
+            }
+        }
+    }
+}
---
+++ SOLID/OpenClosedPrinciple/OpenClosedPrinciple/Program.cs
@@ -0,0 +1,110 @@
+﻿namespace OpenClosedPrinciple
+{
+    public enum Color
+    {
+        Red, Green, Blue
+    }
+    public enum Size
+    {
+        Small, Medium, Large, Yuge
+    }
+    public class Product
+    {
+        public string Name;
+        public Color Color;
+        public Size Size;
+        public Product(string name, Color color, Size size)
+        {
+            if (name == null)
+                throw new ArgumentNullException(paramName: nameof(name));
+            Name = name;
+            Color = color;
+            Size = size;
+        }
+    }
+
+    public class ColorSpec : ISpecification<Product>
+    {
+        private Color color;
+        public ColorSpec(Color color)
+        {
+            this.color = color;
+        }
+        public bool IsSatisfied(Product t)
+        {
+            return t.Color==color;
+        }
+    }
+
+    public class SizeSpec : ISpecification<Product>
+    {
+        private Size size;
+        public SizeSpec(Size size)
+        {
+            this.size = size;
+        }
+        public bool IsSatisfied(Product t)
+        {
+            return t.Size == size;
+        }
+    }
+
+    public class DualSpec<T> : ISpecification<T>
+    {
+        ISpecification<T> first,second;
+        public DualSpec(ISpecification<T> first,ISpecification<T> second)
+        {
+            if (first == null)
+                throw new ArgumentNullException(paramName: nameof(first));
+            this.first = first;
+
+            this.second = second??throw new ArgumentNullException(paramName:nameof(second));
+        }
+        public bool IsSatisfied(T t)
+        {
+            return first.IsSatisfied(t) && second.IsSatisfied(t);
+        }
+    }
+
+    public class BetterFilter : IFilter<Product>
+    {
+        public IEnumerable<Product> Filter(IEnumerable<Product> Items, ISpecification<Product> spec)
+        {
+            foreach (var item in Items)
+            {
+                if(spec.IsSatisfied(item))
+                    yield return item;
+            }
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var apple = new Product("Apple", Color.Green, Size.Small);
+            var Tree = new Product("Tree", Color.Green, Size.Yuge);
+            var Guitar = new Product("Guitar", Color.Blue, Size.Large);
+            Product[] products = new Product[] { apple, Tree, Guitar };
+
+            var pf = new BetterFilter();
+
+            Console.WriteLine("Green products (new):");
+
+            foreach (var p in pf.Filter(products, new ColorSpec(Color.Green)))
+            {
+                Console.WriteLine(p.Name);
+            }
+
+            Console.WriteLine("large blue products (new):");
+
+            ColorSpec blueprods= new ColorSpec(Color.Blue);
+            SizeSpec largeprods= new SizeSpec(Size.Large);
+            foreach (var p in pf.Filter(products, new DualSpec<Product>(blueprods,largeprods)))
+            {
+                Console.WriteLine(p.Name);
+            }
+            
+        }
+    }
+}
