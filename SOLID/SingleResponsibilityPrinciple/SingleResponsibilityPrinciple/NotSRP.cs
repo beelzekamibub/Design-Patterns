b7883706fe7063d3078e4eb2170229b28b377a18
beelzekamibub <chait8126@gmail.com>
2023-08-19 15:00:36 +0530

    single responsibility principle

---
+++ SOLID/SingleResponsibilityPrinciple/SingleResponsibilityPrinciple/NotSRP.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleResponsibilityPrinciple
+{
+    public class Journal
+    {
+        private readonly List<string> entries = new List<string>();
+        private static int count = 0;
+        public int AddEntry(string text)
+        {
+            entries.Add($"{++count}: {text}");
+            return count; //memento
+        }
+        public void ReomveEntry(int index)
+        {
+            entries.RemoveAt(index);
+        }
+
+        public override string ToString()
+        {
+            return string.Join(Environment.NewLine, entries);
+        }
+
+        public void Save(string filename)
+        {
+            File.WriteAllText(filename, ToString());
+        }
+
+        public static Journal Load(string filename)
+        {
+            return new Journal();
+        }
+    }
+    public class NotSRP
+    {
+        public void journalUse()
+        {
+            var j = new Journal();
+            j.AddEntry("i cry");
+            j.AddEntry("i eat bug");
+            Console.WriteLine(j);
+        }
+    }
+}
---
+++ SOLID/SingleResponsibilityPrinciple/SingleResponsibilityPrinciple/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Diagnostics;
+
+namespace SingleResponsibilityPrinciple
+{
+    public class JournalSRP
+    {
+        private readonly List<string> entries = new List<string>();
+        private static int count = 0;
+        public int AddEntry(string text)
+        {
+            entries.Add($"{++count}: {text}");
+            return count; //memento
+        }
+        public void ReomveEntry(int index)
+        {
+            entries.RemoveAt(index);
+        }
+
+        public override string ToString()
+        {
+            return string.Join(Environment.NewLine, entries);
+        }
+    }
+
+    public class Persistence
+    {
+        public void Save(JournalSRP journal,string filename, bool overwrite=false)
+        {
+            if(overwrite ||  !File.Exists(filename)) 
+            {
+                File.WriteAllText(filename, journal.ToString());
+            }
+        }
+
+        public static Journal Load(string filename)
+        {
+            return new Journal();
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var j = new JournalSRP();
+            j.AddEntry("i cry");
+            j.AddEntry("i eat bug");
+            Console.WriteLine(j);
+
+            var p=new Persistence();
+            var filename = @"D:\tmkc.txt";
+            p.Save(j,filename,true);
+            Process.Start(filename);
+        }
+    }
+}
