6d583adb3f40390dfb1af14065d6938541955f5d
beelzekamibub <chait8126@gmail.com>
2023-08-20 01:19:55 +0530

    LSP

---
+++ SOLID/LiskovSubPrinciple/LiskovSubPrinciple/LSPViolate.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiskovSubPrinciple
+{
+    public class Rectanglev
+    {
+        public int width { get; set; }
+        public int height { get; set; }
+        public Rectanglev()
+        {
+
+        }
+        public Rectanglev(int w, int h)
+        {
+            width = w; height = h;
+        }
+        public override string ToString()
+        { return $"{nameof(width)}: {width}, {nameof(height)}: {height}"; }
+    }
+
+    public class Squarev : Rectanglev
+    {
+        public new int width //overriding
+        {
+            set { base.width = base.height = value; }
+        }
+        public new int height //overriding
+        {
+            set { base.width = base.height = value; }
+        }
+    }
+
+    public class LSPViolate
+    {
+        static public int Areav(Rectanglev rc) => rc.height * rc.width;
+        public void UseLSPViolate()
+        {
+            Rectanglev rc = new Rectanglev(2, 3);
+            Console.WriteLine(Areav(rc));
+            Rectanglev square = new Squarev();
+            square.width = 2;
+            Console.WriteLine(Areav(square));
+        }
+    }
+}
---
+++ SOLID/LiskovSubPrinciple/LiskovSubPrinciple/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace LiskovSubPrinciple
+{
+    public class Rectangle
+    {
+        public virtual int width { get; set; }
+        public virtual int height { get; set; }
+        public Rectangle()
+        {
+            
+        }
+        public Rectangle(int w,int h)
+        {
+            width = w; height = h;
+        }
+        public override string ToString()
+        { return $"{nameof(width)}: {width}, {nameof(height)}: {height}";}
+    }
+
+    public class Square : Rectangle 
+    {
+        public override int width //overriding
+        {
+            set{ base.width = base.height = value;}
+        }
+        public override int height //overriding
+        {
+            set { base.width = base.height = value; }
+        }
+    }
+
+    public class Program
+    {
+        static public int Area(Rectangle rc)=>rc.height*rc.width;
+        static void Main(string[] args)
+        {
+            LSPViolate obj = new LSPViolate();
+            obj.UseLSPViolate();
+
+            Console.WriteLine("LSP");
+
+            Rectangle rc = new Rectangle(2,3);
+            Console.WriteLine(Area(rc));
+            Rectangle square = new Square();
+            square.width = 2;
+            Console.WriteLine(Area(square));
+        }
+    }
+}
